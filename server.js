48b544424670296d5c507d8a91ebbc16c92b784d
IssamLynx <hm_bakha@esi.dz>
2021-08-05 12:32:01 +0100

    Connected to database

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express=require('express');
 const dotenv=require('dotenv');
+const connectDB = require('./config/db');
 
-
+//Load env vars
 dotenv.config({path:'./config/config.env'});
 
+//connect to databse 
+connectDB();
+
+//Route files
 const bootcamps=require('./routes/bootcamps');
 
 //Route files
@@ -13,4 +18,11 @@ const app =express();
 app.use('/api/v1/bootcamps',bootcamps);
 
 const PORT=process.env.PORT || 5000;
-app.listen(PORT,console.log(`server is running in ${process.env.NODE_ENV} mode on ${PORT}`));
+const server =app.listen(PORT,console.log(`server is running in ${process.env.NODE_ENV} mode on ${PORT}`));
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection',(err,promise) => {
+    console.log(`Error', ${err.message}`);
+    //Close Server & exit process
+    server.close(() => process.exit(1));
+});
