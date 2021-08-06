e29790febea726ffba0566f76a69809891622a74
IssamLynx <hm_bakha@esi.dz>
2021-08-06 23:13:03 +0100

    adding error response

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,5 +1,5 @@
 const Bootcamp = require('../models/BootCamp');
-
+const ErrorResponse = require('../utils/errorResponse');
 //@desc     GET all bootcamps
 //@route    GET /api/v1/bootcamps
 //@access   Public
@@ -8,7 +8,7 @@ exports.getBootcamps = async(req,res,next) => {
         const bootcamp = await Bootcamp.find();
         res.status(200).json({success:true,data:bootcamp});
     } catch (error) {
-       next(error);
+       next(new ErrorResponse(`Bootcamp not found with the id of ${req.params.id}`,404));
     }
 };
 
@@ -27,7 +27,7 @@ exports.getBootcamp = async(req,res,next) => {
         res.status(200).json({success:true,data:bootcamp});
 
     } catch (error) {
-        next(error);
+        next(new ErrorResponse(`Bootcamp not found with the id of ${req.params.id}`,404));
 
     }
 };
