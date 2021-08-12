26023182461f388cce39bc80a6e5fcfbdcef7a8a
IssamLynx <hm_bakha@esi.dz>
2021-08-12 20:15:10 +0100

    fixed a bug in seeder

--- seeder.js
+++ seeder.js
@@ -8,6 +8,8 @@ dotenv.config({ path: './config/config.env' });
 
 // Load models
 const Bootcamp = require('./models/BootCamp');
+const Course = require('./models/Course');
+
 // Connect to DB
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
