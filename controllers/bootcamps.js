21082c13854507d752c922a4319288ab7490ee77
IssamLynx <hm_bakha@esi.dz>
2021-08-12 20:07:53 +0100

    adding seeder, adding and deleting bootcamps

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,6 +1,6 @@
 const Bootcamp = require('../models/BootCamp');
 const ErrorResponse = require('../utils/errorResponse');
-const asyncHandler = require('./middleware/async');
+const asyncHandler = require('../middleware/async');
 
 //@desc     GET all bootcamps
 //@route    GET /api/v1/bootcamps
