3b257b5759f1600ac3e5ee5811c9a5841a89a275
IssamLynx <hm_bakha@esi.dz>
2021-08-14 13:31:40 +0100

    set up controller and route for authentication

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ connectDB();
 //Route files
 const bootcamps=require('./routes/bootcamps');
 const courses=require('./routes/course');
+const auth=require('./routes/auth');
 
 //Route files
 const app =express();
@@ -28,6 +29,7 @@ app.use(express.static(path.join(__dirname,'public')));
 //Mount routers
 app.use('/api/v1/bootcamps',bootcamps);
 app.use('/api/v1/courses',courses);
+app.use('/api/v1/auth',auth);
 
 app.use(errorHandler);
 
